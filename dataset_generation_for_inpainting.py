ee803302329291c1443016a69e446bc5116fc0fa
musicbyjing <musicbyjing@gmail.com>
2021-01-21 10:55:57 -0500

    Account for RFI beginning and end masks

--- dataset_generation_for_inpainting.py
+++ dataset_generation_for_inpainting.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[54]:
 
 
-# %matplotlib notebook # comment this line when working in VSCode
+get_ipython().run_line_magic('matplotlib', 'inline')
+# jt -t oceans16 -f fira -fs 10 -cellw 1200 -T -N -cursc r
 import aipy, uvtools
 import numpy as np
 import pylab as plt
@@ -28,98 +29,8 @@ from hera_sim import foregrounds, noise, sigchain, rfi, simulate
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
-# # Generate visibility plot
-
-# In[65]:
-
-
-get_ipython().run_line_magic('pinfo', 'sigchain.gen_gains')
-
-
-# In[77]:
-
-
-'''
-Generate one visibility
-'''
-def generate_one_vis(lsts, fqs, bl_len_ns):
-    # point-source and diffuse foregrounds
-    Tsky_mdl = noise.HERA_Tsky_mdl['xx']
-    vis = foregrounds.diffuse_foreground(lsts, fqs, bl_len_ns, Tsky_mdl)
-    vis += foregrounds.pntsrc_foreground(lsts, fqs, bl_len_ns, nsrcs=200)
-
-    # noise
-    tsky = noise.resample_Tsky(fqs,lsts,Tsky_mdl=noise.HERA_Tsky_mdl['xx'])
-    t_rx = 150.
-#     OMEGA_P = (0.72)*np.ones(1024) # from before; fraction of sky telescope is looking at
-    OMEGA_P = noise.bm_poly_to_omega_p(fqs) # default; from the hera_sim docs
-    nos_jy = noise.sky_noise_jy(tsky + t_rx, fqs, lsts, OMEGA_P)
-    vis += nos_jy
-    
-    # crosstalk, gains
-    xtalk = sigchain.gen_whitenoise_xtalk(fqs)
-    g = sigchain.gen_gains(fqs, [1,2,3,4], 20) # default 0.1
-    vis = sigchain.apply_xtalk(vis, xtalk)
-    vis = sigchain.apply_gains(vis, g, (1,2))
-
-    return vis[:, :, np.newaxis]
-
-'''
-Plot one visibility
-MX is max value of color scale in the plot
-DRNG = MX - min value of color scale in the plot
-'''
-def plot_one_vis(vis, ylim, MX, DRNG, figsize):
-    fig = plt.figure(figsize=figsize)
-    ax1 = fig.add_subplot(211)
-    ax2 = fig.add_subplot(212)
-
-    fig.sca(ax1)
-    uvtools.plot.waterfall(vis, mode='log', mx=MX, drng=DRNG)
-    plt.grid(False)  
-    plt.colorbar(label=r"Amplitude [log$_{10}$(V/Jy)]")
-    plt.ylim(0,ylim)
-
-    fig.sca(ax2)
-    uvtools.plot.waterfall(vis, mode='phs')
-    plt.grid(False)
-    plt.colorbar(label="Phase [rad]")
-    plt.ylim(0,ylim)
-    plt.xlabel("Frequency channel")
-
-    fig.text(0.02, 0.5, 'LST [rad]', ha='center', va='center', rotation='vertical')
-
-'''
-Generate n visibilities
-'''
-def generate_vis(n, lsts, fqs, bl_len_ns):
-    res = []
-    for i in range(n):
-        res.append(generate_one_vis(lsts, fqs, bl_len_ns))
-    return np.array(res)
-
-
-# In[ ]:
-
-
-
-
-
-# In[78]:
-
-
-lsts = np.linspace(0, 0.5*np.pi, 1500, endpoint=False) # local sidereal times; start range, stop range, number of snapshots
-# 1500 to match the mask; π/2 ~ 6h
-fqs = np.linspace(.1, .2, 1024, endpoint=False) # frequencies in GHz; start freq, end freq, number of channels
-bl_len_ns = np.array([30.,0,0]) # ENU coordinates
-
-vis = generate_one_vis(lsts, fqs, bl_len_ns)
-print(type(vis))
-print(vis.shape)
-plot_one_vis(vis[:,:,0], 1500, 2.5, 3, (7,7))
-
-
-# # RFI mask
+# # Experimentation
+# ### RFI Mask
 # This is a boolean mask: `True` = RFI, `False` = no RFI
 # 
 # Parameters:
@@ -127,7 +38,7 @@ plot_one_vis(vis[:,:,0], 1500, 2.5, 3, (7,7))
 #   - times match: using range π/2 for lsts in vis plot, with 1500 snapshots
 # - 1024 channels, 100-200 MHz
 
-# In[10]:
+# In[73]:
 
 
 f = h5py.File("mask_HERA.hdf5", "r")
@@ -143,36 +54,46 @@ plt.grid()
 plt.colorbar()
 plt.show()
 print(mask.shape)
-print(mask[()])
+# print(mask[()])
+
+num_jd = mask.shape[0]
+num_freq_channels = mask.shape[1]
 
 
 # ### Generate custom masks
 # 
 # Take horizontal or vertical pixel slice of the mask. Spans of the `True` sections = spans of RFI
 
-# In[11]:
+# In[93]:
 
 
 '''
 Get RFI spans from one row/col
 '''
-def get_RFI_spans(row):
+def get_RFI_spans(row, isRow=False):
     spans = [(key, sum(1 for _ in group)) for key, group in itertools.groupby(row)]
 #     spans = spans[1:-1] # remove first and last element of mask?
     if len(spans) == 1:
         raise Exception("Error: all values in the row/col are True; select another one")
-    return spans
+    if isRow:
+        start = spans[0][1]
+        end = spans[-1][1]
+        print("RFI bookends", start, end)
+        spans = spans[1:-1]
+        return spans, start, end
+    else:
+        return spans
 
-rfi_widths = get_RFI_spans(mask[1434])
+rfi_widths, start_mask, end_mask = get_RFI_spans(mask[1434], isRow=True)
 rfi_heights = get_RFI_spans(mask[:,166])
 
-# print("RFI widths", rfi_widths)
+print("RFI widths", rfi_widths)
 # print("RFI heights", rfi_heights)
 
 
 # Generate randomized mask from spans
 
-# In[12]:
+# In[57]:
 
 
 def plot_mask(mask, mask2, mask3):
@@ -224,16 +145,124 @@ def generate_masks(n, time, freq, widths, heights):
     return np.array(res)
 
 
-# In[13]:
+# In[74]:
 
 
-custom_mask = generate_one_mask(1500, 1024, rfi_widths, rfi_heights, plot=True)
-print(custom_mask)
+custom_mask = generate_one_mask(num_jd, num_freq_channels-(start_mask+end_mask), rfi_widths, rfi_heights, plot=True)
+# print(custom_mask)
+
+
+# ### Visibility waterfall plot
+
+# In[64]:
+
+
+# sigchain.gen_gains?
+
+
+# In[103]:
+
+
+'''
+Generate one visibility
+'''
+def generate_one_vis(lsts, fqs, bl_len_ns):
+    # point-source and diffuse foregrounds
+    Tsky_mdl = noise.HERA_Tsky_mdl['xx']
+    vis = foregrounds.diffuse_foreground(lsts, fqs, bl_len_ns, Tsky_mdl)
+    vis += foregrounds.pntsrc_foreground(lsts, fqs, bl_len_ns, nsrcs=200)
+
+    # noise
+    tsky = noise.resample_Tsky(fqs,lsts,Tsky_mdl=noise.HERA_Tsky_mdl['xx'])
+    t_rx = 150.
+#     OMEGA_P = (0.72)*np.ones(1024) # from before; fraction of sky telescope is looking at; normalizes noise
+    OMEGA_P = noise.bm_poly_to_omega_p(fqs) # default; from the hera_sim docs
+    nos_jy = noise.sky_noise_jy(tsky + t_rx, fqs, lsts, OMEGA_P)
+    vis += nos_jy
+    
+    # crosstalk, gains
+    xtalk = sigchain.gen_whitenoise_xtalk(fqs)
+#     g = sigchain.gen_gains(fqs, [1,2,3,4], 0) # default 0.1 # leave out for now
+    vis = sigchain.apply_xtalk(vis, xtalk)
+#     vis = sigchain.apply_gains(vis, g, (1,2))
+
+    return vis[:, :, np.newaxis]
+
+'''
+Plot one visibility
+MX is max value of color scale in the plot
+DRNG = MX - min value of color scale in the plot
+'''
+def plot_one_vis(vis, ylim, MX, DRNG, figsize):
+    fig = plt.figure(figsize=figsize)
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+
+    fig.sca(ax1)
+    uvtools.plot.waterfall(vis, mode='log', mx=MX, drng=DRNG)
+    plt.grid(False)  
+    plt.colorbar(label=r"Amplitude [log$_{10}$(V/Jy)]")
+    plt.ylim(0,ylim)
+
+    fig.sca(ax2)
+    uvtools.plot.waterfall(vis, mode='phs')
+    plt.grid(False)
+    plt.colorbar(label="Phase [rad]")
+    plt.ylim(0,ylim)
+    plt.xlabel("Frequency channel")
+
+    fig.text(0.02, 0.5, 'LST [rad]', ha='center', va='center', rotation='vertical')
+
+'''
+Generate n visibilities
+'''
+def generate_vis(n, lsts, fqs, bl_len_ns):
+    res = []
+    for i in range(n):
+        res.append(generate_one_vis(lsts, fqs, bl_len_ns))
+    return np.array(res)
+
+
+# Calculate start freq, end freq, and # channels
+
+# In[104]:
+
+
+# Original range: 100 - 200 GHz, 1024 channels
+start_freq = (100 + 100*start_mask/num_freq_channels) / 1000
+
+end_freq = (200 - 100*end_mask/num_freq_channels) / 1000
+num_reduced_channels = num_freq_channels - start_mask - end_mask
+print(start_freq, end_freq, num_reduced_channels)
+
+
+# In[105]:
+
+
+get_ipython().run_line_magic('pinfo', 'foregrounds.diffuse_foreground')
+
+
+# In[106]:
+
+
+lsts = np.linspace(0, 0.5*np.pi, 1500, endpoint=False) # local sidereal times; start range, stop range, number of snapshots
+# 1500 to match the mask; π/2 ~ 6h
+fqs = np.linspace(start_freq, end_freq, 1024, endpoint=False) # frequencies in GHz; start freq, end freq, number of channels
+################### why does this require 1024 channels to get 818 channels??
+# fqs = np.linspace(.1, .2, 1024, endpoint=False) # original
+bl_len_ns = np.array([30.,0,0]) # ENU coordinates
+
+vis = generate_one_vis(lsts, fqs, bl_len_ns)
+vis = vis[:, start_mask:(vis.shape[1]-end_mask)] # to account for start and end mask
+print(vis.shape)
+plot_one_vis(vis[:,:,0], 1500, 2.5, 3, (7,7))
+
+# plt.imshow(np.log(np.abs(vis[:,:,0])))
 
 
 # ### Apply mask to visibility
 
-# In[19]:
+# In[95]:
 
 
 vis[custom_mask == True] = 0
@@ -244,26 +273,27 @@ plot_one_vis(vis[:,:,0], 1500, 2.5, 3, (7,7))
 
 # Create visibilities and masks
 
-# In[234]:
+# In[ ]:
 
 
-lsts = np.linspace(1, 0.5*np.pi, 1500, endpoint=False) # local sidereal times; start range, stop range, number of snapshots
+lsts = np.linspace(0, 0.5*np.pi, 1500, endpoint=False) # local sidereal times; start range, stop range, number of snapshots
 # 1500 to match the mask; π/2 ~ 6h
-fqs = np.linspace(.1, .2, 1024, endpoint=False) # frequencies in GHz; start freq, end freq, number of channels
+fqs = np.linspace(start_freq, end_freq, num_reduced_channels, endpoint=False) # frequencies in GHz; start freq, end freq, number of channels
 bl_len_ns = np.array([30.,0,0]) # ENU coordinates
 
-vis = generate_vis(500, lsts, fqs, bl_len_ns)
-mask = generate_one_mask(1500, 1024, rfi_widths, rfi_heights)
+vis = generate_vis(10, lsts, fqs, bl_len_ns)
+mask = generate_one_mask(1500, 1024-(start_mask+end_mask), rfi_widths, rfi_heights)
 
 
 # Create data and labels
 
-# In[235]:
+# In[101]:
 
 
 data = vis.copy()
 for i, v in enumerate(data):
     v[mask == True] = 0
+
 # print(np.count_nonzero(train_dataset[0]==0)) # check number of 0's in a given vis (to check if mask worked)
 
 labels = vis
@@ -292,7 +322,7 @@ np.save("labels.npy", labels)
 
 # Separate train and test sets
 
-# In[ ]:
+# In[102]:
 
 
 from sklearn.model_selection import train_test_split

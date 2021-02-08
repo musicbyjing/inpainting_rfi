2fc255a2132ae2607e74b156d2f11a4fba4b3dcd
Jing Liu <jingliu6@cedar1.cedar.computecanada.ca>
2021-02-08 05:42:56 -0800

    Update jobs

--- jobs/job_predict.sh
+++ jobs/job_predict.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-acliu
-#SBATCH --time=0-12:00
-#SBATCH --gres=gpu:v100l:1
-#SBATCH --cpus-per-task=6
+#SBATCH --time=0-00:30
 #SBATCH --mem=128G
 #SBATCH --mail-user=jing.liu6@mail.mcgill.ca
 #SBATCH --mail-type=ALL
 #SBATCH --output=%N-%j.out
 
 module load cuda cudnn
-python3 scripts/train_model.py --max_epochs 500 --id 1612458166_500_examples_1_masks
+python3 scripts/predict.py --id 1612458166_500_examples_1 --model-name weights.best.hdf5

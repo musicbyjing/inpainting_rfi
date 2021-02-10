591adaff0af9b91f2692b0f09720665dcbae82f4
Jing Liu <jingliu6@cedar5.cedar.computecanada.ca>
2021-02-10 14:42:20 -0800

    Refactor

--- scripts/predict.py
+++ scripts/predict.py
@@ -8,9 +8,21 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import backend as K
 
-import aipy, uvtools
-from hera_sim import foregrounds, noise, sigchain, rfi, simulate
+from utils import plot_one_vis
 
+### Currently, chooses a random image from X_test and makes a prediction. 
+### Saves the original, predicted, and ground truth as png's.
+
+def load_model(model_name):
+    folder = "."
+    return keras.models.load_model(os.path.join(folder, f"{model_name}"), custom_objects={'masked_MSE': masked_MSE})
+
+def load_data(file_id):
+    folder = "data"
+    data = np.load(os.path.join(folder, f"{file_id}_Xtest.npy"))
+    label = np.load(os.path.join(folder, f"{file_id}_ytest.npy"))
+    return data, label
+    
 def predict(model, input):
     return model.predict(np.array([input, ]))[0] # since model.predict() needs an array
 
@@ -24,53 +36,6 @@ def masked_MSE(y_true, y_pred):
         yp = yp[mask == True]
     loss_val = K.mean(K.square(y_pred - y_true))
     return loss_val
-
-def load_model(model_name):
-    folder = "."
-    return keras.models.load_model(os.path.join(folder, f"{model_name}"), custom_objects={'masked_MSE': masked_MSE})
-
-def load_data(file_id):
-    folder = "data"
-    data = np.load(os.path.join(folder, f"{file_id}_Xtest.npy"))
-    label = np.load(os.path.join(folder, f"{file_id}_ytest.npy"))
-    return data, label
-
-def plot_one_vis(input_vis, ylim, MX, DRNG, figsize, title, filepath):
-    '''
-    Plot one visibility waterfall plot and save it
-    MX is max value of color scale in the plot
-    DRNG = MX - min value of color scale in the plot
-    '''
-    # consolidate real and imag channels of vis if necessary
-    print(input_vis.shape)
-    if input_vis.ndim > 2: 
-        vis = np.zeros((input_vis.shape[0], input_vis.shape[1]), dtype = 'complex128')
-        vis[:, :] = input_vis[:, :, 0] + input_vis[:, :, 1]*1j
-    else:
-        vis = input_vis
-
-    # plot
-    fig = plt.figure(figsize=figsize)
-    ax1 = fig.add_subplot(211)
-    ax2 = fig.add_subplot(212)
-
-    fig.sca(ax1)
-    uvtools.plot.waterfall(vis, mode='log', mx=MX, drng=DRNG)
-    plt.suptitle(title)
-    plt.grid(False)  
-    plt.colorbar(label=r"Amplitude [log$_{10}$(V/Jy)]")
-    plt.ylim(0,ylim)
-
-    fig.sca(ax2)
-    uvtools.plot.waterfall(vis, mode='phs')
-    plt.grid(False)
-    plt.colorbar(label="Phase [rad]")
-    plt.ylim(0,ylim)
-    plt.xlabel("Frequency channel")
-
-    fig.text(0.02, 0.5, 'LST [rad]', ha='center', va='center', rotation='vertical')
-
-    plt.savefig(filepath)
 
 def get_prediction(model, data, label):
     '''
@@ -112,7 +77,6 @@ def main():
     # get predictions
     get_prediction(model, data, label)
     print("predict.py completed.")
-
 
 if __name__ == "__main__":
     main()
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,10 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import aipy, uvtools
 
 def load_dataset(file_id):
+    ''' Load dataset, consisting of data, labels, and masks '''
     folder = "data"
     data = np.load(os.path.join(folder, f"{file_id}_dataset.npy"))
     labels = np.load(os.path.join(folder, f"{file_id}_labels.npy"))
@@ -11,6 +13,7 @@ def load_dataset(file_id):
     return data, labels, mask
 
 def plot_loss(history, file_id):
+    ''' Plot learning curves '''
     plt.plot(history.history['loss'], label='loss')
     plt.plot(history.history['val_loss'], label='val_loss')
     plt.xlabel('Epoch')
@@ -18,3 +21,40 @@ def plot_loss(history, file_id):
     plt.legend()
     plt.grid(True)
     plt.savefig(f"{file_id}.png")
+
+def plot_one_vis(input_vis, ylim, MX, DRNG, figsize, title, filepath):
+    '''
+    Plot one visibility waterfall plot and save it
+    MX is max value of color scale in the plot
+    DRNG = MX - min value of color scale in the plot
+    '''
+    # consolidate real and imag channels of vis if necessary
+    print(input_vis.shape)
+    if input_vis.ndim > 2: 
+        vis = np.zeros((input_vis.shape[0], input_vis.shape[1]), dtype = 'complex128')
+        vis[:, :] = input_vis[:, :, 0] + input_vis[:, :, 1]*1j
+    else:
+        vis = input_vis
+
+    # plot
+    fig = plt.figure(figsize=figsize)
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+
+    fig.sca(ax1)
+    uvtools.plot.waterfall(vis, mode='log', mx=MX, drng=DRNG)
+    plt.suptitle(title)
+    plt.grid(False)  
+    plt.colorbar(label=r"Amplitude [log$_{10}$(V/Jy)]")
+    plt.ylim(0,ylim)
+
+    fig.sca(ax2)
+    uvtools.plot.waterfall(vis, mode='phs')
+    plt.grid(False)
+    plt.colorbar(label="Phase [rad]")
+    plt.ylim(0,ylim)
+    plt.xlabel("Frequency channel")
+
+    fig.text(0.02, 0.5, 'LST [rad]', ha='center', va='center', rotation='vertical')
+
+    plt.savefig(filepath)

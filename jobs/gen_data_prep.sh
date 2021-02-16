b972410b45b5c50c8d1eec228a6a4ce8d05f1604
Jing Liu <jingliu6@cedar1.cedar.computecanada.ca>
2021-02-16 08:47:51 -0800

    Update jobs

--- jobs/gen_data_prep.sh
+++ jobs/gen_data_prep.sh
@@ -4,4 +4,7 @@
 #SBATCH --mem=4G
 #SBATCH --mail-user=jing.liu6@mail.mcgill.ca
 #SBATCH --mail-type=ALL
+
+source ~/HERA_ENV/bin/activate
+module load scipy-stack
 python3 scripts/generate_dataset.py --n_masks 3 --n_examples 5
